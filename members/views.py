9abef4a06396b41ae938b30f8a1b48e574c64885
Alkarakully <alkarakully@gmail.com>
2024-12-26 15:21:36 -0600

    Updating the app to get dynamic msg from the environ

--- members/views.py
+++ members/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import socket
+import socket, os
 from django.template import loader
 
 # Create your views here.
 
 def home(request):
     hostname = socket.gethostname()
-    hosts = {"hostname": hostname}
+    custome_header = os.environ.get("custom_header")
+    hosts = {"hostname": hostname, "custom_header": custome_header}
     template = loader.get_template("index.html")
     return HttpResponse(template.render(hosts))
 
 def members(request):
     hostname = socket.gethostname()
-    hosts = {"hostname": hostname}
+    custome_header = os.environ("custom_header")
+    hosts = {"hostname": hostname, "custom_header": custome_header}
     template = loader.get_template("index.html")
     return HttpResponse(template.render(hosts))
