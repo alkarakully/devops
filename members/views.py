454995d3a22761f15f6f1d2dfa69bf897c0f0411
Alkarakully <alkarakully@gmail.com>
2024-12-26 19:09:18 -0600

    Get header from file in opt dir

--- members/views.py
+++ members/views.py
@@ -7,16 +7,16 @@ from django.template import loader
 
 def home(request):
     hostname = socket.gethostname()
-    with open("/opt/custom_header", "r") as header:
-        custome_header = header.readlines()[0]
+    with open("/opt/custom_header", "r") as header_file:
+        custome_header = header_file.readlines()[0]
     hosts = {"hostname": hostname, "custom_header": custome_header}
     template = loader.get_template("index.html")
     return HttpResponse(template.render(hosts))
 
 def members(request):
     hostname = socket.gethostname()
-    with open("/opt/custom_header", "r") as header:
-        custome_header = header.readlines()[0]
+    with open("/opt/custom_header", "r") as header_file:
+        custome_header = header_file.readlines()[0]
     hosts = {"hostname": hostname, "custom_header": custome_header}
     template = loader.get_template("index.html")
     return HttpResponse(template.render(hosts))
