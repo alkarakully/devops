89e60701aa4152289b008bc587d34db9cb855f96
Alkarakully <alkarakully@gmail.com>
2024-12-31 22:57:45 -0600

    Add validation to check if the custom header exists

--- members/views.py
+++ members/views.py
@@ -7,6 +7,13 @@ from django.template import loader
 
 def home(request):
     hostname = socket.gethostname()
+    file_path = "/opt/custom_header"
+    default_content = "This is the default header content\n"
+    if not os.path.exists(file_path):
+        # If the file does not exist, create it and write the default content
+        with open(file_path, "w") as header_file:
+            header_file.write(default_content)
+            
     with open("/opt/custom_header", "r") as header_file:
         custome_header = header_file.readlines()[0]
     hosts = {"hostname": hostname, "custom_header": custome_header}
@@ -15,6 +22,13 @@ def home(request):
 
 def members(request):
     hostname = socket.gethostname()
+    file_path = "/opt/custom_header"
+    default_content = "This is the default header content\n"
+    if not os.path.exists(file_path):
+        # If the file does not exist, create it and write the default content
+        with open(file_path, "w") as header_file:
+            header_file.write(default_content)
+
     with open("/opt/custom_header", "r") as header_file:
         custome_header = header_file.readlines()[0]
     hosts = {"hostname": hostname, "custom_header": custome_header}
